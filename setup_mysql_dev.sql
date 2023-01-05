e886e08e1359543457cc6ae2b545f958246fe9d2
dkkiooko <kithekadankioko@gmail.com>
2023-01-05 12:49:05 +0300

    changed keywords to all caps

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,7 @@
 -- prepare a MySQL server for this project
--- create user with privileges on one database
 
-CREATE database IF NOT EXISTS hbnb_dev_db;
-CREATE user IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performace_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
