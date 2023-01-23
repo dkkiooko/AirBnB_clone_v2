c0828d8e3a3de7ca3ec6ebc50abb77603c2a7f0e
dkkiooko <kithekadankioko@gmail.com>
2023-01-24 01:26:26 +0300

    removed unnecessary blocks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 # set up web servers for deployment of webstatic
 WHERE="/etc/nginx/sites-available/default"
-if ! dpkg -s nginx; then
-    apt-get update
-    if apt-get -y install nginx; then
-        ADD="\\\tlocation \/redirect_me {\n\t\treturn 301 http:dankioko.tech;\n\t}\n"
-        HEADER="\\\n\tadd_header X-Served-By \$hostname;"
-        echo "Hello World!" | sudo tee /usr/share/nginx/html/index.html
-        sed -i "30i $ADD" $WHERE
-        sed -i "29i $HEADER" $WHERE
-        new="\t\terror_page 404 @my404"
-        sudo sed -i "/^\tlocation \/ {$/a \\$new" /etc/nginx/sites-available/default
-        new="location @my404 {\n\t\tdefault_type text/html;\n\t\treturn 404 \"Ceci n'est pas une page\";\n\t}"
-        sudo sed -i "/^\tserver_name localhost;$/a \\\n\\t$new" /etc/nginx/sites-available/default
-    fi
-fi
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 cp /etc/nginx/sites-available/default{,.orig}
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared/
-echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ data/web_static/current
-chown -R ubuntu:ubuntu /data/
-ADD2="location http://dankioko.tech/hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sed -i "/^\tserver_name localhost;$/a \\\n\\t$ADD2" $WHERE
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo "this is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" $WHERE
 service nginx restart
