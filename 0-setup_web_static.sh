7a36d9b7ee88dc01fe0468a5fccc0615a5409b1b
dkkiooko <kithekadankioko@gmail.com>
2023-01-24 21:37:33 +0300

    set up a web static on your servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ cp /etc/nginx/sites-available/default{,.orig}
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 echo "this is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" $WHERE
+sudo sed -i "38i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" $WHERE
 service nginx restart
