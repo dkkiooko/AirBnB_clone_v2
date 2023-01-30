1156a1f4583668fbf0a95f375057d1b3e27027a4
dkkiooko <kithekadankioko@gmail.com>
2023-01-30 21:02:20 +0300

    trying to configure a second machine with a different version of ubuntu, plut it will be great to have a laptop option as opposed to the desktop

--- fabfile.py
+++ fabfile.py
@@ -7,12 +7,4 @@ from os.path import isdir
 
 def do_pack():
     """ generate a tgz archive """
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception:
-        return
+    pass
